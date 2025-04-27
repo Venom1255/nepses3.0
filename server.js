e5aa04ec1823928b12792839ca17cc09efb515ff
kamranabbas1255 <kamranbagoro@gmail.com>
2025-04-27 21:04:27 +0500

    fix: use process.env.PORT for Railway deployment

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = 5050;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-}); 
+});
